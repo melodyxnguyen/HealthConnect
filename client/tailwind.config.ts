29a6dbfb5f7f47cdd9543526183e071a5f480aa2
Melody Nguyen <melodyxnguyen@gmail.com>
2025-04-04 13:17:25 -0400

    trying to fix UX

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,7 +2,10 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./index.html", "./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       borderRadius: {
